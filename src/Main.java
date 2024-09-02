9007aad127a8f2823c1a3651370511afed84945d
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-02 13:25:17 +0400

    add class transport and start truck class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
+//        ArrayList<Byte>  numbers = new ArrayList<>();
+//        numbers.add((byte) 1);
+//        numbers.add((byte) 2);
+//        numbers.add((byte) 32);
+//
+//        LinkedList<Float> numbers= new LinkedList<>();
+//        numbers.add(1.2f);
+//        System.out.println(numbers);
+
+
+
+        Transport bmw = new Transport(250.0f, 1500, "red", new byte[] {0, 0, 0});
+        Transport truck = new Transport(2500, new byte[] {100, 0, 100});
+        bmw.getValues();
+
 
     }
 }
--- src/Transport.java
+++ src/Transport.java
@@ -1,6 +1,43 @@
 public class Transport {
-    private String color;
+
     private float speed;
     private int weight;
+    private String color;
+    private byte[] coordinates;
 
+
+    public Transport(float speed, int weight, String color, byte[] coordinates){
+
+        System.out.println("object created");
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinates = coordinates;
+        System.out.println(getValues());
+
+    }
+
+    public Transport(int weight, byte[] coordinates){
+        System.out.println("object created");
+        this.weight = weight;
+        this.coordinates = coordinates;
+        System.out.println(getValues());
+    }
+
+
+    public void setValues(float speed, int weight, String color, byte[] coordinates){
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinates = coordinates;
+
+    }
+
+
+    public String getValues(){
+
+        String info = ("Speed: " + speed + " Weight: " + weight + " Color: " + color + " Coordinates: " + coordinates);
+
+        return info;
+    }
 }
