7cdaec5e50f042548bf96f037606e888f184a9a9
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-04 22:09:10 +0400

    add .iml adn add gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
-public class Main {
+public classMain {
     public static void main(String[] args) {
 
 //        ArrayList<Byte>  numbers = new ArrayList<>();
