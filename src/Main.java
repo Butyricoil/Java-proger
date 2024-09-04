916208df4c5801b01a135ea7e2635d30e4284863
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-04 22:25:07 +0400

    BugFix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
-public classMain {
+public class Main {
     public static void main(String[] args) {
 
 //        ArrayList<Byte>  numbers = new ArrayList<>();
